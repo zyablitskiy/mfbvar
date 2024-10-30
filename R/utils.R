94ae5064274fd54f6d51b2d61b2d660f1b0b454d
zyablitskiy <136595297+zyablitskiy@users.noreply.github.com>
2024-10-30 10:23:03 +0300

    Update utils.R
    Corrected computation of initial error variance for low-frequency series

--- R/utils.R
+++ R/utils.R
@@ -3,11 +3,13 @@
 compute_error_variances <- function(Y) {
   n_vars <- ncol(Y)
   error_variance <- rep(NA, n_vars)
+  Y_arima <- lapply(Y, function(x) x)
   for (i in 1:n_vars) {
     success <- NULL
     init_order <- 4
+    if (prior$freq[i]=="q") {Y_arima[[i]] <- Y_arima[[i]][seq(1, length(Y_arima[[i]]), 3)]}
     for (ar_order in init_order:1) {
-      error_variance[i] <- tryCatch(arima(na.omit(Y[,i]), order = c(ar_order, 0, 0), method = "ML")$sigma2,
+      error_variance[i] <- tryCatch(arima(na.omit(Y_arima[[i]]), order = c(ar_order, 0, 0), method = "ML", optim.method="L-BFGS-B")$sigma2,
                                     error = function(cond) NA)
       if (!is.na(error_variance[i])) {
         break
