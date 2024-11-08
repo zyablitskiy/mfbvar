9ee85f47802c201d5e8e3ecbd9237e29c34d5449
zyablitskiy <136595297+zyablitskiy@users.noreply.github.com>
2024-11-08 13:19:06 +0300

    Update utils.R

--- R/utils.R
+++ R/utils.R
@@ -7,15 +7,18 @@ compute_error_variances <- function(Y) {
   for (i in 1:n_vars) {
     success <- NULL
     init_order <- 4
-    if (prior$freq[i]=="q") {Y_arima[[i]] <- Y_arima[[i]][seq(1, length(Y_arima[[i]]), 3)]}
+    if (prior$freq[i]=="q") {Y_arima[[i]] <- na.omit(Y_arima[[i]][seq(which(!is.na(Y_arima[[i]]))[1], length(Y_arima[[i]]), 3)]})
     for (ar_order in init_order:1) {
-      error_variance[i] <- tryCatch(arima(na.omit(Y_arima[[i]]), order = c(ar_order, 0, 0), method = "ML", optim.method="L-BFGS-B")$sigma2,
+      error_variance[i] <- tryCatch(arima(Y_arima[[i]], 
+                                          order = c(ar_order, 0, 0), 
+                                          method = "ML", 
+                                          optim.method="L-BFGS-B")$sigma2,
                                     error = function(cond) NA)
       if (!is.na(error_variance[i])) {
         break
       } else {
         if (init_order < 1) {
-          error_variance[i] <- var(na.omit(Y[,i]))
+          error_variance[i] <- var(Y_arima[[i]])
         }
       }
     }
